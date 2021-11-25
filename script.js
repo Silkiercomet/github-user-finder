91a914b56a366a6d70ec5de9113a01984da81ef5
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-11-25 07:54:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 const input = document.querySelector("#devname")
 const btn = document.querySelector(".search-btn")
 
-const getUsers = async function(){
-
-    const url = `https://api.github.com/search/users?q=octocat`
+async function getUsers(){
+    user = input.value;
+    //basic user info
+    const url = `https://api.github.com/users/${user}`
     const result = await fetch(url)
-    let state = await result.json()
-    console.log(state)
+    .then(state =>  state.json())
+    .catch(error => console.log(error))
+    return result
 
-    let repos = await fetch("https://api.github.com/users/octocat/repos"),
-    repoResult = await repos.json()
-    //console.log( repoResult.length )
-
-    let followers = await fetch("https://api.github.com/users/octocat/followers+total_count"),
-    followResult = await followers.json()
-    console.log( followResult )
-    return state.items[0];
+    
 }
-getUsers()
+
 const printUser = (promise) => {
-    promise.then( function(){
+    promise.then( function(promise){
+        /*user avatar */ document.querySelector("#user_img").src = promise.avatar_url;
+        /*user name*/ document.querySelector(".username").textContent = promise.name;
+        /*user link*/ document.querySelector(".user__profile_link").setAttribute("href",`${promise.html_url}`);
+        document.querySelector("#link__name").innerText = promise.login;
+        /*user bio*/
+        (promise.bio == null)? document.querySelector("#user__bio").textContent = "there's no bio" : (document.querySelector("#user__bio").textContent = promise.bio);
+        /*user details, repos, etc */
+        document.querySelector("#repos").textContent = promise.public_repos;
+        document.querySelector("#followers").textContent = promise.followers;
+        document.querySelector("#following").textContent = promise.following;
+        
 
     })
 }
+const compose = (f,g) => (data) => f(g(data));
+
+btn.addEventListener("click", function(){
+    compose(printUser,getUsers)()
+})
